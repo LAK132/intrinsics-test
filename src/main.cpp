3239c1fa9285f39cc6f9b4c80cca59caa92a0206
LAK132 <lakridesagain@gmail.com>
2020-10-31 03:34:49 +1030

    Another commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include "lak/array.hpp"
 #include "lak/debug.hpp"
-#include "lak/intrin.hpp"
+#include "lak/profile.hpp"
+#include "lak/vec_intrin.hpp"
 
 #include <iostream>
+
+struct scoped_timer
+{
+  const lak::astring name;
+  const uint64_t start;
+  scoped_timer(const lak::astring &timer_name)
+  : name(timer_name), start(lak::performance_counter())
+  {
+  }
+
+  ~scoped_timer()
+  {
+    const uint64_t end = lak::performance_counter();
+    const f64_t diff   = static_cast<f64_t>(end - start);
+    std::cout << name << ": " << std::fixed
+              << (diff / lak::performance_frequency()) << "sec\n";
+  }
+};
+
+#define SCOPED_TIMER(NAME)                                                    \
+  const auto UNIQUIFY(SCOPED_TIMER_) = scoped_timer(NAME)
 
 int main()
 {
@@ -11,7 +34,7 @@ int main()
 
   auto instructions = lak::instruction_set::get();
 
-  std::cout << std::boolalpha;
+  std::cout << std::boolalpha << std::fixed;
   std::cout << "MMX? " << instructions.MMX << "\n";
   std::cout << "SSE? " << instructions.SSE << "\n";
   std::cout << "SSE2? " << instructions.SSE2 << "\n";
@@ -25,9 +48,56 @@ int main()
   std::cout << "AVX512PF? " << instructions.AVX512PF << "\n";
   std::cout << "AVX512ER? " << instructions.AVX512ER << "\n";
   std::cout << "AVX512CD? " << instructions.AVX512CD << "\n";
+
+  std::cout << "\n";
+
+  lak::array<lak::vec4f64_t> vectors;
+  vectors.resize(32'000'000);
+  for (auto &vec : vectors) vec = lak::vec4f64_t{1.0, 2.0, 3.0, 4.0};
+
+  lak::vec4f64_t out;
+  lak::vec4f64_t *volatile out_ptr = &out;
+
+  for (int j = 0; j < 2; ++j)
+  {
+    for (int i = 0; i < 5; ++i)
+    {
+      lak::vec4f64_t max = {0.0, 0.0, 0.0, 0.0};
+      {
+        SCOPED_TIMER("control");
+        for (auto &vec : vectors)
+        {
+          max.x = std::max(max.x, vec.x);
+          max.y = std::max(max.y, vec.y);
+          max.z = std::max(max.z, vec.z);
+          max.w = std::max(max.w, vec.w);
+        }
+      }
+      *out_ptr = max;
+    }
+
+    std::cout << "\n";
+
+    for (int i = 0; i < 5; ++i)
+    {
+      lak::m256d_t max = _mm256_set1_pd(0.0);
+      {
+        SCOPED_TIMER("intrinsic");
+        for (auto &vec : vectors)
+        {
+          max = _mm256_max_pd(lak::to_wide(vec), max);
+        }
+      }
+      *out_ptr = lak::to_vec4(max);
+    }
+
+    std::cout << "\n";
+  }
 }
 
 #include "../inc/lak/src/debug.cpp"
 #include "../inc/lak/src/intrin.cpp"
+#include "../inc/lak/src/memmanip.cpp"
+#include "../inc/lak/src/profile.cpp"
 #include "../inc/lak/src/strconv.cpp"
 #include "../inc/lak/src/unicode.cpp"
